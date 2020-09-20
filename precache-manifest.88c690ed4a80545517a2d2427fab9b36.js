a7b6425062efb8da3fb797bd0f115c46102c7ec2
Anshultilondiya <43814313+Anshultilondiya@users.noreply.github.com>
2020-09-20 19:54:19 +0530

    Updates

---
+++ precache-manifest.88c690ed4a80545517a2d2427fab9b36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44139ff302b51f9848b403751cf81aec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6267bd4b0f69beb3988",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f3ab9e666b14251740c5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a6267bd4b0f69beb3988",
+    "url": "/static/js/2.8350f835.chunk.js"
+  },
+  {
+    "revision": "5d05ac6106bee1827c7ede4c0b7b01c1",
+    "url": "/static/js/2.8350f835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ab9e666b14251740c5",
+    "url": "/static/js/main.449d97e3.chunk.js"
+  },
+  {
+    "revision": "ca5d4258bd552ace1181",
+    "url": "/static/js/runtime-main.14a4eb31.js"
+  }
+]);
