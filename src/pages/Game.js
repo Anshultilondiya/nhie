ed5287be4238613422d1415a11857f5051786ea3
Anshultilondiya <43814313+Anshultilondiya@users.noreply.github.com>
2020-09-20 19:35:55 +0530

    trying Deployment

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -30,7 +30,7 @@ class Game extends Component {
         received: '',
     };
     store = this.props.appStore;
-    endpoint = 'https://blissful-hoover-013892.netlify.app';
+    endpoint = '/';
     socket = io(this.endpoint);
     componentDidMount() {
         const { name, room } = queryString.parse(window.location.search);
